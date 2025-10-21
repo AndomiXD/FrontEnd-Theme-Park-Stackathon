608bed5869dbfe1cfe9f1de04002d204cde48743
AndomiXD <vintin4some@gmail.com>
2025-10-21 19:55:15 +0300

    show all coasters and details of one coaster

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,7 +1,30 @@
+import { useState, useEffect } from "react"
+import axios from "axios"
+import { useParams } from "react-router-dom"
+
 const Details = () => {
+  const [coaster, setCoaster] = useState({})
+
+  const { _id } = useParams()
+
+  useEffect(() => {
+    const getOneCoaster = async () => {
+      try {
+        let response = await axios.get(`http://localhost:3000/coasters/${_id}`)
+        setCoaster(response.data)
+      } catch (err) {
+        console.log(err)
+      }
+    }
+    getOneCoaster()
+  }, [])
+
+  console.log(coaster)
   return (
     <>
-      <p>this</p>
+      <p>{coaster.name}</p>
+      <p>{coaster.details}</p>
+      <p>{coaster.price}</p>
     </>
   )
 }
