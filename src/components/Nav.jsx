f2fe9cf9a3966fb175222fe5ee841c7002c26d57
fatima-mohmd <fatoomm473@gmail.com>
2025-10-21 10:27:14 +0300

    review form

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,7 +2,7 @@ import { NavLink } from "react-router-dom"
 const Nav=()=>{
   return(<nav>
     <div>
-      <NavLink to="/">Home</NavLink>
+      <NavLink to="/">Home | </NavLink>
       <NavLink to="/new">New</NavLink>
       </div>
 
--- src/components/Review.jsx
+++ src/components/Review.jsx
@@ -1,4 +1,16 @@
 const Review=()=>{
-  return(<></>)
+  return(<>
+  <form>
+    <input type="text" placeholder="Review the roller coaster"/>
+    <br />
+    <label >Rate the roller coaster from 5</label>
+    <br />
+    <input type="radio" value="1"/><label>1</label>
+    <input type="radio" value="2"/><label>2</label>
+    <input type="radio" value="3"/><label>3</label>
+    <input type="radio" value="4"/><label>4</label>
+    <input type="radio" value="5"/><label>5</label>
+  </form>
+  </>)
 }
 export default Review
