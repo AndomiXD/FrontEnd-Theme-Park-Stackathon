f47c8db928dc7005ef3d8de95e7cc2a09d8d9b9c
fatima-mohmd <fatoomm473@gmail.com>
2025-10-21 10:08:40 +0300

    starting new form

--- src/components/New.jsx
+++ src/components/New.jsx
@@ -1,6 +1,40 @@
-const New=()=>{
-  return(<>
-  <p>new</p>
-  </>)
+const New = () => {
+  return (
+    <div>
+      <h1>Add A New Roller Coaster</h1>
+      <form>
+        <input
+          type="text"
+          // value={boat.name}
+          // onChange={handleChange}
+          // name={"name"}
+          placeholder="name"
+        />
+        <br />
+        <input
+          type="text"
+          // value={boat.img}
+          // onChange={handleChange}
+          // name={"img"}
+          placeholder="details"
+        />
+        <input
+          type="text-area"
+          // value={boat.description}
+          // onChange={handleChange}
+          // name={"description"}
+          // placeholder={"description"}
+        />
+        <input
+          type="text"
+          // value={boat.price}
+          // onChange={handleChange}
+          // name={"price"}
+          // placeholder={"price"}
+        />
+        <button>Submit</button>
+      </form>
+    </div>
+  )
 }
 export default New
