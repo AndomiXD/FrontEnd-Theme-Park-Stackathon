e74d4e3c7049d6cb47b56d024f781bf35f300c6c
AndomiXD <vintin4some@gmail.com>
2025-10-21 22:46:49 +0300

    details of rollercoaster comments and ratings, delete button too

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,11 +1,20 @@
 import { useState, useEffect } from "react"
 import axios from "axios"
 import { useParams } from "react-router-dom"
+import { useNavigate } from "react-router-dom"
 
 const Details = () => {
   const [coaster, setCoaster] = useState({})
 
   const { _id } = useParams()
+  const navigate = useNavigate()
+  const refresh = false
+
+  const handleDelete = async () => {
+    const response = await axios.delete(`http://localhost:3000/coasters/${_id}`)
+    navigate("/")
+    refresh = true
+  }
 
   useEffect(() => {
     const getOneCoaster = async () => {
@@ -17,14 +26,24 @@ const Details = () => {
       }
     }
     getOneCoaster()
-  }, [])
+    refresh = false
+  }, [refresh])
 
   console.log(coaster)
   return (
     <>
-      <p>{coaster.name}</p>
-      <p>{coaster.details}</p>
-      <p>{coaster.price}</p>
+      <button onClick={handleDelete}> Delete Coaster</button>
+      <h1>Roller Coaster Name: {coaster.name}</h1>
+      <h2>Details: {coaster.details}</h2>
+      <h3>Price: ${coaster.price}</h3>
+      {coaster.reviews?.map((x, index) => (
+        <div key={index}>
+          <h4>
+            Review {index + 1}: {x.comment}
+          </h4>
+          <h5>Rating: {x.rating}</h5>
+        </div>
+      ))}
     </>
   )
 }
